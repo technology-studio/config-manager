2d5c2088386a8cc7beed4e96c8d859b7c8812a0c
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2018-11-13 14:06:06 +0100

    Add tests for ConfigManager. Add config change subsciptions.

---
+++ __tests__/ConfigManagerTest.js
@@ -0,0 +1,72 @@
+/**
+ * @Author: Rostislav Simonik <rostislav.simonik>
+ * @Date:   2018-01-09T00:56:40+01:00
+ * @Email:  rostislav.simonik@technologystudio.sk
+ * @Copyright: Technology Studio
+ * @flow
+ */
+
+'use strict'; // eslint-disable-line
+
+import { ConfigManager } from '../lib'
+test('shoud pass', () => {
+  expect(true).toBe(true)
+})
+
+const INITIAL_CONFIG = {
+  notTouchedValue: 123,
+  toBeTouchedValue: 234,
+}
+
+const NEW_VALUE_CONFIG = {
+  newValue: 456,
+}
+const WITH_NEW_VALUE_CONFIG = {
+  ...INITIAL_CONFIG,
+  ...NEW_VALUE_CONFIG,
+}
+
+const MERGE_CONFIG = {
+  toBeTouchedValue: 890,
+}
+const MERGED_CONFIG = {
+  ...INITIAL_CONFIG,
+  ...MERGE_CONFIG,
+}
+
+describe('ConfigManager basic tests', () => {
+  var configManager
+
+  beforeEach(() => {
+    configManager = new ConfigManager(INITIAL_CONFIG)
+  })
+
+  test('should contain the same reference when same configuration is updated', () => {
+    configManager.update(INITIAL_CONFIG)
+    expect(configManager.config).toBe(INITIAL_CONFIG)
+  })
+
+  test('should merge with new value', () => {
+    configManager.update(NEW_VALUE_CONFIG)
+    expect(configManager.config).toStrictEqual(WITH_NEW_VALUE_CONFIG)
+  })
+
+  test('should merge with override', () => {
+    configManager.update(MERGE_CONFIG)
+    expect(configManager.config).toStrictEqual(MERGED_CONFIG)
+  })
+
+  test('should not call onChange', () => {
+    const onChange = jest.fn()
+    configManager.subscribe(onChange)
+    configManager.update(INITIAL_CONFIG)
+    expect(onChange).toHaveBeenCalledTimes(0)
+  })
+
+  test('should call onChange', () => {
+    const onChange = jest.fn()
+    configManager.subscribe(onChange)
+    configManager.update(NEW_VALUE_CONFIG)
+    expect(onChange).toHaveBeenCalledTimes(1)
+  })
+})
